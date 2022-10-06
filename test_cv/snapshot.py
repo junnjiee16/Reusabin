df9e9094f60aad68a20789fdb6596f5694498d41
bevanpoh <bevanpoh@gmail.com>
2022-10-06 16:18:50 +0800

    add take_photo function

--- test_cv/snapshot.py
+++ test_cv/snapshot.py
@@ -1,15 +1,32 @@
 # from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
-from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
-from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch, ImageFileCreateEntry, Region
+from azure.cognitiveservices.vision.customvision.prediction import (
+    CustomVisionPredictionClient,
+)
+from azure.cognitiveservices.vision.customvision.training.models import (
+    ImageFileCreateBatch,
+    ImageFileCreateEntry,
+    Region,
+)
 from msrest.authentication import ApiKeyCredentials
 import os, time, uuid
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageFont
+import cv2
 import numpy as np
+
+
+def take_photo(path):
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    cv2.imwrite(path, frame)
+    cap.release()
+
 
 def predict_image(img_path):
     # Replace with valid values
-    prediction_endpoint = "https://foursknspaihack-prediction.cognitiveservices.azure.com/"
+    prediction_endpoint = (
+        "https://foursknspaihack-prediction.cognitiveservices.azure.com/"
+    )
     # training_key = "5f84e1195ead47cd920e88d09984e7ef"
     prediction_key = "fcdd642e9cf94df885f770f60ba51959"
     # prediction_resource_id = "/subscriptions/9bd23583-8d4c-4886-9aa8-d0cd3172afb0/resourceGroups/4SKN_SPAI_Hackathon/providers/Microsoft.CognitiveServices/accounts/foursknspaihack-Prediction"
@@ -18,17 +35,29 @@ def predict_image(img_path):
 
     # credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
     credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
-    prediction_client = CustomVisionPredictionClient(endpoint=prediction_endpoint, credentials=credentials)
+    prediction_client = CustomVisionPredictionClient(
+        endpoint=prediction_endpoint, credentials=credentials
+    )
 
     image_file = os.path.abspath(img_path)
-    print('Detecting objects in image: ' + image_file)
-    
+    print("Detecting objects in image: " + image_file)
+    image = Image.open(image_file)
+
     with open(image_file, mode="rb") as image_data:
         results = prediction_client.detect_image(ProjectID, ModelName, image_data)
 
-    for prediction in results.predictions: 
-        if (prediction.probability*100) > 50:
-            print(prediction.tag_name + ": {0:.2f}%".format(prediction.probability * 100))
+    for prediction in results.predictions:
+        if (prediction.probability * 100) > 50:
+            print(
+                prediction.tag_name + ": {0:.2f}%".format(prediction.probability * 100)
+            )
 
 
-predict_image('./test_cv/photo/can.jpg')
+def main():
+    path = "./test_cv/photo/can.jpg"
+    take_photo(path)
+    predict_image(path)
+
+
+if __name__ == "__main__":
+    main()
